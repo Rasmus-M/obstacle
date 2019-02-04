2b5d1fa9dc7554505f7f5a53559981f6b12b3441
Rasmus-M <rasmus.moustgaard@gmail.com>
2019-02-04 22:41:44 +0100

    Tool chain

--- mag/lz4map.bat
+++ mag/lz4map.bat
@@ -1,5 +1,9 @@
+java -jar ..\tools\asm2bin.jar maps.a99 map1.bin MD1 MC2
 ..\tools\lz4.exe -9 -f map1.bin map1.lz4
-copy /Y map1.lz4 ..\src\
+..\tools\sfk.exe partcopy map1.lz4 -fromto 11 -8 map1trim.lz4 -yes
+copy /Y map1trim.lz4 ..\src\
 
+java -jar ..\tools\asm2bin.jar maps.a99 map2.bin MD2
 ..\tools\lz4.exe -9 -f map2.bin map2.lz4
-copy /Y map2.lz4 ..\src\
+..\tools\sfk.exe partcopy map2.lz4 -fromto 11 -8 map2trim.lz4 -yes
+copy /Y map2trim.lz4 ..\src\
--- make.bat
+++ make.bat
@@ -1,3 +1,7 @@
+cd mag
+call lz4map.bat
+cd ..
+
 xas99.py -R -S -L obstacles.lst -b src/obstacles.a99
 del obstacles-c.bin
 ren obstacles_6000.bin obstacles-c.bin

8b8eff7883813e8deb6ed65ce3177696d84dff21
Rasmus-M <rasmus.moustgaard@gmail.com>
2019-02-06 22:12:19 +0100

    Four maps

--- mag/lz4map.bat
+++ mag/lz4map.bat
@@ -3,7 +3,17 @@ java -jar ..\tools\asm2bin.jar maps.a99 map1.bin MD1 MC2
 ..\tools\sfk.exe partcopy map1.lz4 -fromto 11 -8 map1trim.lz4 -yes
 copy /Y map1trim.lz4 ..\src\
 
-java -jar ..\tools\asm2bin.jar maps.a99 map2.bin MD2
+java -jar ..\tools\asm2bin.jar maps.a99 map2.bin MD2 MC3
 ..\tools\lz4.exe -9 -f map2.bin map2.lz4
 ..\tools\sfk.exe partcopy map2.lz4 -fromto 11 -8 map2trim.lz4 -yes
 copy /Y map2trim.lz4 ..\src\
+
+java -jar ..\tools\asm2bin.jar maps.a99 map3.bin MD3 MC4
+..\tools\lz4.exe -9 -f map3.bin map3.lz4
+..\tools\sfk.exe partcopy map3.lz4 -fromto 11 -8 map3trim.lz4 -yes
+copy /Y map3trim.lz4 ..\src\
+
+java -jar ..\tools\asm2bin.jar maps.a99 map4.bin MD4
+..\tools\lz4.exe -9 -f map4.bin map4.lz4
+..\tools\sfk.exe partcopy map4.lz4 -fromto 11 -8 map4trim.lz4 -yes
+copy /Y map4trim.lz4 ..\src\

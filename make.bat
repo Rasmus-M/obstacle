b2eb710958637aaa953c1b72410f5640d8987c3b
Rasmus-M <rasmus.moustgaard@gmail.com>
2019-02-13 19:16:56 +0100

    Two cart banks

--- make.bat
+++ make.bat
@@ -2,6 +2,10 @@ cd mag
 call lz4map.bat
 cd ..
 
-xas99.py -R -S -L obstacles.lst -b src/obstacles.a99
-del obstacles-c.bin
-ren obstacles_6000.bin obstacles-c.bin
+xas99.py -R -S -L obstacle-course.lst -b src/obstacle-course.a99
+
+tools\pad.exe obstacle-course_6000_b0.bin obstacle-c.bin 8192
+tools\pad.exe obstacle-course_6000_b1.bin obstacle-d.bin 8192
+
+copy /b obstacle-c.bin + obstacle-d.bin obstacle-9.bin
+copy /b obstacle-d.bin + obstacle-c.bin obstacle-8.bin

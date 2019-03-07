2698e3dfb99520b9149a840be67eceabae679fee
Rasmus-M <rasmus.moustgaard@gmail.com>
2019-03-07 21:04:27 +0100

    Use new banking features of XAS99

--- make.bat
+++ make.bat
@@ -4,8 +4,11 @@ cd ..
 
 xas99.py -R -S -L obstacle-course.lst -b src/obstacle-course.a99
 
-tools\pad.exe obstacle-course_6000_b0.bin obstacle-c.bin 8192
-tools\pad.exe obstacle-course_6000_b1.bin obstacle-d.bin 8192
+copy /b obstacle-course_6000.bin + obstacle-course_*_b0.bin obstacle-course_b0.bin
+copy /b obstacle-course_6000.bin + obstacle-course_*_b1.bin obstacle-course_b1.bin
+
+tools\pad.exe obstacle-course_b0.bin obstacle-c.bin 8192
+tools\pad.exe obstacle-course_b1.bin obstacle-d.bin 8192
 
 copy /b obstacle-c.bin + obstacle-d.bin obstacle-9.bin
 copy /b obstacle-d.bin + obstacle-c.bin obstacle-8.bin

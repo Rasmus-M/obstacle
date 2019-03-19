975b1e10afa8c722661941b140bdea77a8bb7d9b
Rasmus-M <rasmus.moustgaard@gmail.com>
2019-03-19 19:46:01 +0100

    Prepare new levels

--- maps/lz4map.bat
+++ maps/lz4map.bat
@@ -18,7 +18,27 @@ java -jar ..\tools\asm2bin.jar maps.a99 map3.bin MD4 MC5
 ..\tools\sfk.exe partcopy map3.lz4 -fromto 11 -8 map3trim.lz4 -yes
 copy /Y map3trim.lz4 ..\src\
 
-java -jar ..\tools\asm2bin.jar maps.a99 map4.bin MD5
+java -jar ..\tools\asm2bin.jar maps.a99 map4.bin MD5 MC6
 ..\tools\lz4.exe -9 -f map4.bin map4.lz4
 ..\tools\sfk.exe partcopy map4.lz4 -fromto 11 -8 map4trim.lz4 -yes
 copy /Y map4trim.lz4 ..\src\
+
+java -jar ..\tools\asm2bin.jar maps.a99 map5.bin MD6 MC7
+..\tools\lz4.exe -9 -f map5.bin map5.lz4
+..\tools\sfk.exe partcopy map5.lz4 -fromto 11 -8 map5trim.lz4 -yes
+copy /Y map5trim.lz4 ..\src\
+
+java -jar ..\tools\asm2bin.jar maps.a99 map6.bin MD7 MC8
+..\tools\lz4.exe -9 -f map6.bin map6.lz4
+..\tools\sfk.exe partcopy map6.lz4 -fromto 11 -8 map6trim.lz4 -yes
+copy /Y map6trim.lz4 ..\src\
+
+java -jar ..\tools\asm2bin.jar maps.a99 map7.bin MD8 MC9
+..\tools\lz4.exe -9 -f map7.bin map7.lz4
+..\tools\sfk.exe partcopy map7.lz4 -fromto 11 -8 map7trim.lz4 -yes
+copy /Y map7trim.lz4 ..\src\
+
+java -jar ..\tools\asm2bin.jar maps.a99 map8.bin MD9
+..\tools\lz4.exe -9 -f map8.bin map8.lz4
+..\tools\sfk.exe partcopy map8.lz4 -fromto 11 -8 map8trim.lz4 -yes
+copy /Y map8trim.lz4 ..\src\

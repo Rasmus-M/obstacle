d1c0fd79802bc221f5b2880320b1b0606b48ed81
Rasmus-M <rasmus.moustgaard@gmail.com>
2019-04-18 17:14:46 +0200

    Disk version

---
+++ makedisk.bat
@@ -0,0 +1,8 @@
+IF EXIST obstacle.dsk GOTO :dskok
+xdm99.py obstacle.dsk --initialize DSSD -n OBSTACLE
+:dskok
+
+xas99.py -R -S -L obstacle-disk.lst -D disk -i src/obstacle-course-disk.a99 -o OBSTACLE1
+
+xdm99.py obstacle.dsk -a OBSTACLE1
+xdm99.py obstacle.dsk -a OBSTACLE2

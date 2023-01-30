8d57a07374109ed9d6c1be52d36bf8a1f3c2b561
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2023-01-30 17:29:01 +0100

    Update build scripts

--- make.bat
+++ make.bat
@@ -6,24 +6,20 @@ java -jar tools\asm2bin.jar src/sprite-patterns.a99 sprite-patterns.bin
 tools\lz4.exe -9 -f sprite-patterns.bin sprite-patterns.lz4
 tools\sfk.exe partcopy sprite-patterns.lz4 -fromto 11 -8 src\sprite-patterns.lz4 -yes
 
-xas99.py -R -S -L obstacle-course.lst -b src/obstacle-course.a99
+del sprite-patterns.bin
+del sprite-patterns.lz4
+
+xas99.py -R -S -q -L obstacle-course.lst -b src/obstacle-course.a99
 @IF %ERRORLEVEL% NEQ 0 GOTO :end
 
-copy /b obstacle-course_6000.bin + obstacle-course_*_b0.bin obstacle-course_b0.bin
-copy /b obstacle-course_6000.bin + obstacle-course_*_b1.bin obstacle-course_b1.bin
+tools\pad.exe obstacle-course_b0.bin obstacle-d.bin 8192
+tools\pad.exe obstacle-course_b1.bin obstacle-c.bin 8192
 
-del obstacle-course_6000.bin
-del obstacle-course_*_b0.bin
-del obstacle-course_*_b1.bin
+del obstacle-course_b0.bin
+del obstacle-course_b1.bin
 
-tools\pad.exe obstacle-course_b0.bin obstacle-c.bin 8192
-tools\pad.exe obstacle-course_b1.bin obstacle-d.bin 8192
-
-rem del obstacle-course_b0.bin
-rem del obstacle-course_b1.bin
-
-copy /b obstacle-c.bin + obstacle-d.bin obstacle-9.bin
-copy /b obstacle-d.bin + obstacle-c.bin obstacle-8.bin
+copy /b obstacle-d.bin + obstacle-c.bin obstacle-9.bin
+copy /b obstacle-c.bin + obstacle-d.bin obstacle-8.bin
 
 WHERE jar
 @IF %ERRORLEVEL% NEQ 0 GOTO :end
--- makedisk.bat
+++ makedisk.bat
@@ -2,7 +2,7 @@ IF EXIST obstacle.dsk GOTO :dskok
 xdm99.py obstacle.dsk --initialize DSSD -n OBSTACLE
 :dskok
 
-xas99.py -R -S -L obstacle-disk.lst -D disk -i src/obstacle-course-disk.a99 -o OBSTACLE1
+xas99.py -R -S -q -L obstacle-disk.lst -D disk -i src/obstacle-course-disk.a99 -o OBSTACLE1
 
 xdm99.py obstacle.dsk -a LOAD
 xdm99.py obstacle.dsk -a OBSTACLE1
